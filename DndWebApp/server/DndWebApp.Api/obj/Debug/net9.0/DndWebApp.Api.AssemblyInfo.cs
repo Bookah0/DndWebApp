c0613d39783a3685a0e85124db80c8ca269fb1e8
Anthon Wirback <anthon.wirback@gmail.com>
2025-10-26 02:48:13 +0100

    assembly

--- DndWebApp/server/DndWebApp.Api/obj/Debug/net9.0/DndWebApp.Api.AssemblyInfo.cs
+++ DndWebApp/server/DndWebApp.Api/obj/Debug/net9.0/DndWebApp.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DndWebApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+641961eebe0f4069591c3c59498388bc5f9749b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d3ce06f3565a7531071bbafe7162a1510acf5e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DndWebApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DndWebApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

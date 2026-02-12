545a9733ba9e1226612c0533998dd7d88549bbd3
Anthon Wirback <anthon.wirback@gmail.com>
2026-02-12 16:03:42 +0100

    reorganized DTOs

--- src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e254a6cc3d2cd8aae4b9f1a8fb9f2067cc31796a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bdecce51ed5ac8b09d3214daab1903f9858dd84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
